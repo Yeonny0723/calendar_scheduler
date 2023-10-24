17e77e57aa19a14257db13c1e81d3898c59390d3
Juyeon Kim <kkjuyeon@gmail.com>
2023-10-24 19:43:39 +0900

    feat: list view 무한스크롤

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -32,10 +32,25 @@ class _HomeScreenState extends State<HomeScreen> {
           SizedBox(height: 8.0), // -- margin 역할
           TodayBanner(selectedDay: DateTime.now(), scheduleCount: 3),
           SizedBox(height: 8.0),
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: ScheduleCard(
-                  startTime: 8, endTime: 11, content: '밥먹기', color: Colors.red))
+          Expanded(
+            child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                // -- 리스트를 보여줄 수 있는 위젯 ListView
+                // -- safearea를 벗어나지 않고도 5개 넘치면 scroll 할 수 있음
+                // -- 자동 Infinite scroll 구현하여 리소스 상 유리함!
+                child: ListView.builder(
+                  itemCount: 20,
+                  itemBuilder: (context, index) {
+                    // -- 아이템 5개 반복문
+                    return ScheduleCard(
+                      startTime: 8,
+                      endTime: 11,
+                      content: '밥먹기 ${index}',
+                      color: Colors.red,
+                    );
+                  },
+                )),
+          )
         ],
       ),
     ));
