4082c0a18e156e922091fe49a0bd49fd49ee8e7a
Juyeon Kim <kkjuyeon@gmail.com>
2023-10-24 19:22:44 +0900

    feat: schedule card 컴포넌트

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:calendar_scheduler/component/calendar.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
 import 'package:calendar_scheduler/today_banner.dart';
 import 'package:flutter/material.dart';
 
@@ -29,7 +30,12 @@ class _HomeScreenState extends State<HomeScreen> {
             onDaySelected: onDaySelected,
           ),
           SizedBox(height: 8.0), // -- margin 역할
-          TodayBanner(selectedDay: DateTime.now(), scheduleCount: 3)
+          TodayBanner(selectedDay: DateTime.now(), scheduleCount: 3),
+          SizedBox(height: 8.0),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ScheduleCard(
+                  startTime: 8, endTime: 11, content: '밥먹기', color: Colors.red))
         ],
       ),
     ));
